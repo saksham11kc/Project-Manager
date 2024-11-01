1925833412188fcd07ac7188eb58cda3b9ef2028
saksham11kc <96900198+saksham11kc@users.noreply.github.com>
2024-11-01 12:35:36 +0545

    Create Basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProject = { ...projectData, id: Math.random() };
+
+      return {
+        ...prevState,
+        projects: prevState.projects.concat(newProject),
+      };
+    });
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+        {content}
+      </main>
     </>
   );
 }
